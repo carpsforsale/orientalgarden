8962b84c0b8184b557be3bdbe9d5a5336eaeb42c
carpsforsale <moonmustdieproductions@gmail.com>
2025-03-09 15:49:06 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,18 +9,18 @@ function createRandomGridLines() {
         const gridLine = document.createElement('div');
         gridLine.classList.add('grid-line');
         
-        const xPosition = Math.random() * 100; 
+        // Set a random position
+        const xPosition = Math.random() * 100;
         const yPosition = Math.random() * 100;
         
-        gridLine.style.width = `${Math.random() * 3 + 1}px`;
-        gridLine.style.height = `${Math.random() * 3 + 1}px`; 
-        
         if (Math.random() > 0.5) {
+            // Vertical line
             gridLine.style.width = '2px';
             gridLine.style.height = `${Math.random() * 100 + 20}%`;
             gridLine.style.left = `${xPosition}%`;
             gridLine.style.top = `${yPosition}%`;
         } else {
+            // Horizontal line
             gridLine.style.height = '2px';
             gridLine.style.width = `${Math.random() * 100 + 20}%`;
             gridLine.style.top = `${yPosition}%`;
